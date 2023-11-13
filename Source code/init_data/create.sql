05de7638860da78c9d8cc9260be1f4659c5d045b
annasouk <112012508+annasouk@users.noreply.github.com>
2023-11-13 13:33:40 -0700

    Update create.sql with auto updating ID

--- Source code/init_data/create.sql
+++ Source code/init_data/create.sql
@@ -1,6 +1,7 @@
 DROP TABLE IF EXISTS users CASCADE;
 CREATE TABLE users(
-    username VARCHAR(50) PRIMARY KEY,
+    username VARCHAR(50) NOT NULL,
     password CHAR(60) NOT NULL,
-    id INT NOT NULL
+    id SERIAL PRIMARY KEY
 );
+
