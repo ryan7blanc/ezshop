996d4c32be05528c71366f4845fa7857f8e176b1
Louis Blanc <lobl1307@colorado.edu>
2023-11-17 09:56:26 -0700

    added cart and product to sql

--- Source code/init_data/create.sql
+++ Source code/init_data/create.sql
@@ -5,3 +5,18 @@ CREATE TABLE users(
     id SERIAL PRIMARY KEY
 );
 
+DROP TABLE IF EXISTS products CASCADE;
+CREATE TABLE products(
+    name VARCHAR(50) NOT NULL,
+    description VARCHAR(200) NOT NULL,
+    price INT NOT NULL,
+    review INT NOT NULL,
+    id SERIAL PRIMARY KEY NOT NULL
+);
+
+DROP TABLE IF EXISTS cart CASCADE;
+CREATE TABLE cart(
+    FOREIGN KEY (id) REFERENCES users (id) ON DELETE CASCADE,
+    product Integer[99],
+    FOREIGN KEY (EACH ELEMENT OF )
+);
